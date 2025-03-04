8b477f244b598e04625c9eb65ce5e046e6b4c547
Catchooo <br.m.airways@gmail.com>
2025-03-04 14:21:34 +0200

    Create platformer.py

---
+++ platformer.py
@@ -0,0 +1,10 @@
+import pygame
+
+pygame.init()
+
+width = 800
+height = 600
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption("Platformer")
+
+background_color = (0, 0, 0)
